840b63d6aab34c68cf5dd927c046fb040cf646fc
omar faruk <69864169+whereisfarukk@users.noreply.github.com>
2023-06-12 21:54:41 +0600

    Add files via upload

---
+++ Semester 3-1/Software Architecture and design pattern/DesignPattern/src/Strategy/Client.java
@@ -0,0 +1,13 @@
+package Behavioral_design_pattern.Strategy;
+
+public class Client {
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 5, 2, 66, 7, 9};
+        Sorter sorter = new Sorter();
+        sorter.setSortingStrategy(new BubbleSort());
+        sorter.sort(arr);
+
+        sorter.setSortingStrategy(new SelectionSort());
+        sorter.sort(arr);
+    }
+}
