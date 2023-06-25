b3e76cf296207ee5085b0ecc95839020bc09ba83
omar faruk <69864169+whereisfarukk@users.noreply.github.com>
2023-06-25 23:36:26 +0600

    Delete Semester 3-1/Software Architecture and design pattern/DesignPattern/src/Behavioral_design_pattern/Iterator directory

--- Semester 3-1/Software Architecture and design pattern/DesignPattern/src/Behavioral_design_pattern/Iterator/Iterator.java
+++
@@ -1,7 +0,0 @@
-package Behavioral_design_pattern.Iterator;
-
-public interface Iterator {
-    boolean hasNext();
-    String current();
-    void next();
-}
--- Semester 3-1/Software Architecture and design pattern/DesignPattern/src/Behavioral_design_pattern/Iterator/TV.java
+++
@@ -1,5 +0,0 @@
-package Behavioral_design_pattern.Iterator;
-
-public interface TV {
-    Iterator getIterator();
-}
